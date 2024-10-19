74fa3e5668daf8ddcd6739e2faad3be115ec561f
guerfala <mohamedtaher.guerfala@esprit.tn>
2024-10-19 15:19:37 +0100

    config database

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,13 @@
-// lib/db.ts
-import { createPool } from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 
-const pool = createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'superiamo',
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,    // Your database host
+  user: process.env.DB_USER,    // Your database user
+  password: process.env.DB_PASS, // Your database password
+  database: process.env.DB_NAME, // Your database name
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;
